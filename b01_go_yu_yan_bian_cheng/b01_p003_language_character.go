008d14dd67248c03054876e987efdb79306fc70b
zxwtry <zxwtry@qq.com>
2017-12-31 16:59:45 +0800

    add b01 p020

--- b01_go_yu_yan_bian_cheng/b01_p003_language_character.go
+++ b01_go_yu_yan_bian_cheng/b01_p003_language_character.go
@@ -1,9 +1,9 @@
 package b01_go_yu_yan_bian_cheng
 
 import (
-	"fmt"
-	"reflect"
-	"unsafe"
+    "fmt"
+    "reflect"
+    "unsafe"
 )
 /*
 #include<stdio.h>
@@ -19,34 +19,34 @@ import "C"
 
 
 /*
-	Go语言最主要的特性：
-	1，	自动垃圾回收
-	2，	丰富的内置类型
-	3，	函数多返回值
-	4，	错误处理
-	5，	匿名函数和闭包
-	6，	类型和接口
-	7，	并发编程
-	8，	反射
-	9，	语言交互性
+    Go语言最主要的特性：
+    1，	自动垃圾回收
+    2，	丰富的内置类型
+    3，	函数多返回值
+    4，	错误处理
+    5，	匿名函数和闭包
+    6，	类型和接口
+    7，	并发编程
+    8，	反射
+    9，	语言交互性
  */
 
 
  /*
- 	匿名函数和闭包：
- 	在Go语言中，所有函数也是值类型，可以作为参数传递。
- 	下列代码定义了一个叫f的匿名函数，可以随意对该匿名函数变量进行传递和调用
- 	f := func(x, y int) int {
- 		return x - y;
- 	}
+    匿名函数和闭包：
+    在Go语言中，所有函数也是值类型，可以作为参数传递。
+    下列代码定义了一个叫f的匿名函数，可以随意对该匿名函数变量进行传递和调用
+    f := func(x, y int) int {
+        return x - y;
+    }
  */
 
 
 /*
-	类型和接口：
-	1， 不支持继承和重载
-	2， 支持最基本的类型组合
-	3， 非“侵入式”接口
+    类型和接口：
+    1， 不支持继承和重载
+    2， 支持最基本的类型组合
+    3， 非“侵入式”接口
 
  */
 
@@ -55,92 +55,92 @@ type Bird struct {
 }
 
 func (b *Bird) Fly() {
-	fmt.Println("飞。。。")
+    fmt.Println("飞。。。")
 }
 
 type IFly interface {
-	Fly()
+    Fly()
 }
 
 func P003Bird()  {
-	var fly IFly = new (Bird)
-	fly.Fly()
-	// Bird类型实现时，没有声明与接口IFly的关系
-	// 接口和类型可以直接转换
-	// 接口的定义不需要在类型定义之前
-	/*
-		输出：
-			飞。。。
-	 */
+    var fly IFly = new (Bird)
+    fly.Fly()
+    // Bird类型实现时，没有声明与接口IFly的关系
+    // 接口和类型可以直接转换
+    // 接口的定义不需要在类型定义之前
+    /*
+        输出：
+            飞。。。
+     */
 }
 
 
 /*
-	并发编程：
-	1， goroutine使用消息传递来共享内存，不是裸用OS的并发机制
-	2， 函数调用前使用关键字go，让该函数以goroutine方式执行
-	3， goroutine是一种比线程更加轻盈、更省资源的协程
-	4， Go用通道实现CSP（通信顺序进程），进行跨goroutine通信
-	5， Go读写锁：一个进程的所有goroutine运行在同一内存地址空间
+    并发编程：
+    1， goroutine使用消息传递来共享内存，不是裸用OS的并发机制
+    2， 函数调用前使用关键字go，让该函数以goroutine方式执行
+    3， goroutine是一种比线程更加轻盈、更省资源的协程
+    4， Go用通道实现CSP（通信顺序进程），进行跨goroutine通信
+    5， Go读写锁：一个进程的所有goroutine运行在同一内存地址空间
  */
 
 func sum(values[] int, resultChan chan int)  {
-	sum := 0
-	for _, value := range values {
-		sum += value
-	}
-	resultChan <- sum	// 将计算结果发送到channel中
+    sum := 0
+    for _, value := range values {
+        sum += value
+    }
+    resultChan <- sum	// 将计算结果发送到channel中
 }
 
 func P003Sum() {
-	values := [] int {1, 2, 3, 4, 5, 6, 7, 8, 9}
-	resultChan := make(chan int, 2)
-	go sum(values[: len(values) / 2], resultChan)
-	go sum(values[len(values) / 2 :], resultChan)
-	sum1, sum2 := <-resultChan, <-resultChan //接收结果
-	fmt.Println("result:", sum1, sum2, sum1 + sum2)
-	/*
-		输出：
-		result: 35 10 45
-	 */
+    values := [] int {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    resultChan := make(chan int, 2)
+    go sum(values[: len(values) / 2], resultChan)
+    go sum(values[len(values) / 2 :], resultChan)
+    sum1, sum2 := <-resultChan, <-resultChan //接收结果
+    fmt.Println("result:", sum1, sum2, sum1 + sum2)
+    /*
+        输出：
+        result: 35 10 45
+     */
 }
 
 
 /*
-	反射：
-	1，	没有内置类型工厂，无法通过类型字符串创建对象实例
-	2，	使用场景：对象的序列化
+    反射：
+    1，	没有内置类型工厂，无法通过类型字符串创建对象实例
+    2，	使用场景：对象的序列化
  */
 
 type CatReflect struct {
-	Name string
-	LifeExpect int
+    Name string
+    LifeExpect int
 }
 
 func MainReflect()  {
-	cat := &CatReflect{"bosi", 1}
-	catRef := reflect.ValueOf(cat).Elem()
-	typeOfT := catRef.Type()
-	for i := 0; i < catRef.NumField(); i ++ {
-		f := catRef.Field(i)
-		fmt.Printf("%d: %s %s = %v\n", i, typeOfT.Field(i).Name,
-			f.Type(), f.Interface())
-	}
-	/*
-		0: Name string = bosi
-		1: LifeExpect int = 1
-	 */
+    cat := &CatReflect{"bosi", 1}
+    catRef := reflect.ValueOf(cat).Elem()
+    typeOfT := catRef.Type()
+    for i := 0; i < catRef.NumField(); i ++ {
+        f := catRef.Field(i)
+        fmt.Printf("%d: %s %s = %v\n", i, typeOfT.Field(i).Name,
+            f.Type(), f.Interface())
+    }
+    /*
+        0: Name string = bosi
+        1: LifeExpect int = 1
+     */
 }
 
 
 /*
-	语言交互性：
-	1，	Go重用现有C模块   -->  Cgo
+    语言交互性：
+    1，	Go重用现有C模块   -->  Cgo
  */
 
 func P003Cgo()  {
-	str := C.CString("Hello World")
-	C.puts(str)
-	C.free(unsafe.Pointer(str))
+    str := C.CString("Hello World")
+    C.puts(str)
+    C.free(unsafe.Pointer(str))
 }
 
--- b01_go_yu_yan_bian_cheng/b01_p020_var.go
+++ b01_go_yu_yan_bian_cheng/b01_p020_var.go
@@ -7,51 +7,87 @@ package b01_go_yu_yan_bian_cheng
  */
 
 /*
-	变量声明：
-		var v1 int
-		var v2 string
-		var v3 [10]int
-		var v4 []int
-		var v5 struct {
-			f int
-		}
-		var v6 *int		// 指针
-		var v7 map[string] int  	// map key为string, value为int
-		var v8 func(a int) int
-		var (
-			v1 int
-			v2 string
-		)
+    变量声明：
+        var v1 int
+        var v2 string
+        var v3 [10]int
+        var v4 []int
+        var v5 struct {
+            f int
+        }
+        var v6 *int		// 指针
+        var v7 map[string] int  	// map key为string, value为int
+        var v8 func(a int) int
+        var (
+            v1 int
+            v2 string
+        )
 
-	变量初始化：
-		var v1 int = 10
-		var v2 = 10
-		v3 := 10
-	(:=  进行变量声明和初始化工作)
+    变量初始化：
+        var v1 int = 10
+        var v2 = 10
+        v3 := 10
+    (:=  进行变量声明和初始化工作)
 
-	初始化编译出错：
-		var i int
-		i := 2
+    初始化编译出错：
+        var i int
+        i := 2
 
-	多重赋值：
-		(交换i和j)
-		i,j = j,i
+    多重赋值：
+        (交换i和j)
+        i,j = j,i
 
-	匿名变量：
-		func GetName() (firstName, lastName, nickName string) {
-			return "May", "Chan", "Chibi Maruko"
-		}
-		_, _, nickName := GetName()
-		只想获得nickName
+    匿名变量：
+        func GetName() (firstName, lastName, nickName string) {
+            return "May", "Chan", "Chibi Maruko"
+        }
+        _, _, nickName := GetName()
+        只想获得nickName
 
-	常量定义：
-		const PI float64 = 3.14
-		const zero = 0.0
-		const (
-			size int64 = 1024
-			eof = -1
-		)
+    常量定义：
+        const PI float64 = 3.14
+        const zero = 0.0
+        const (
+            size int64 = 1024
+            eof = -1
+        )
+        const u, v float32 = 0, 3		// u=0.0 v=3.0常量的多重赋值
+        const a, b, c = 3, 4, "foo"
+        // a = 3, b = 4, c = "foo"
 
+        Go的常量定义可以限定常量类型，但不是必需的。
+        如果定义常量没有指定类型，和字面常量一样，也是无类型常量
 
+        常量定义的右值，也可以是一个在编译期运算的表达式
+        const mask = 1 << 3
+        由于常量的赋值是一个编译期行为，
+        右值不能出现任何需要运行期才能得出结果的表达式。
+        const Home = os.GetEnv("HOME")		//这里会有编译错误
+        os.GetEnv()只有在运行期才能知道返回结果，编译期不能确定
+
+ */
+
+/*
+    预定义常量：
+        Go语言预定义了这些常量：true、false、iota
+        iota比较特殊，可以被认为是一个可被编译器修改的变量
+        在每一个const关键字出现时被重置为0
+        然后再下一个const出现之前，每出现一次iota，其所代表的数字会自动增1
+        const (				// iota被重设为0
+            c0 = iota		// c0 == 0
+            c1 = iota		// c1 == 1
+            c2 = iota		// c2 == 2
+            c3 = iota		// c3 == 3
+        )
+        const (					// iota被重设为0
+            c0 = 1 << iota		// c0 == 1
+            c1 = 1 << iota		// c1 == 2
+            c2 = 1 << iota		// c2 == 4
+            c3 = 1 << iota		// c3 == 8
+        )
+        const (
+            u 	      = 1 << iota		// a == 1
+            v float64 = 1 << iota		// b == 1
+        )
 
  */
