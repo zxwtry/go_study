30c3e68dc97a4e3b0b05e25b084eea2062737593
zxwtry <zxwtry@qq.com>
2018-01-01 09:46:12 +0800

    add b01 p020

--- b01_go_yu_yan_bian_cheng/b01_p020_var.go
+++ b01_go_yu_yan_bian_cheng/b01_p020_var.go
@@ -15,8 +15,8 @@ package b01_go_yu_yan_bian_cheng
         var v5 struct {
             f int
         }
-        var v6 *int		// 指针
-        var v7 map[string] int  	// map key为string, value为int
+        var v6 *int     // 指针
+        var v7 map[string] int      // map key为string, value为int
         var v8 func(a int) int
         var (
             v1 int
@@ -51,7 +51,7 @@ package b01_go_yu_yan_bian_cheng
             size int64 = 1024
             eof = -1
         )
-        const u, v float32 = 0, 3		// u=0.0 v=3.0常量的多重赋值
+        const u, v float32 = 0, 3       // u=0.0 v=3.0常量的多重赋值
         const a, b, c = 3, 4, "foo"
         // a = 3, b = 4, c = "foo"
 
@@ -62,7 +62,7 @@ package b01_go_yu_yan_bian_cheng
         const mask = 1 << 3
         由于常量的赋值是一个编译期行为，
         右值不能出现任何需要运行期才能得出结果的表达式。
-        const Home = os.GetEnv("HOME")		//这里会有编译错误
+        const Home = os.GetEnv("HOME")      //这里会有编译错误
         os.GetEnv()只有在运行期才能知道返回结果，编译期不能确定
 
  */
@@ -73,21 +73,60 @@ package b01_go_yu_yan_bian_cheng
         iota比较特殊，可以被认为是一个可被编译器修改的变量
         在每一个const关键字出现时被重置为0
         然后再下一个const出现之前，每出现一次iota，其所代表的数字会自动增1
-        const (				// iota被重设为0
-            c0 = iota		// c0 == 0
-            c1 = iota		// c1 == 1
-            c2 = iota		// c2 == 2
-            c3 = iota		// c3 == 3
+        const (             // iota被重设为0
+            c0 = iota       // c0 == 0
+            c1 = iota       // c1 == 1
+            c2 = iota       // c2 == 2
+            c3 = iota       // c3 == 3
         )
-        const (					// iota被重设为0
-            c0 = 1 << iota		// c0 == 1
-            c1 = 1 << iota		// c1 == 2
-            c2 = 1 << iota		// c2 == 4
-            c3 = 1 << iota		// c3 == 8
+        const (	                // iota被重设为0
+            c0 = 1 << iota      // c0 == 1
+            c1 = 1 << iota      // c1 == 2
+            c2 = 1 << iota      // c2 == 4
+            c3 = 1 << iota      // c3 == 8
         )
         const (
-            u 	      = 1 << iota		// a == 1
-            v float64 = 1 << iota		// b == 1
+            u 	      = iota * 42       // u == 0
+            v float64 = iota * 42       // v == 42.0
+            v float64 = iota * 42       // v == 84.0
         )
+        const x = iota          // x == 0 (const出现，iota置0)
+        const y = iota          // y == 0 (const出现，iota置0)
 
+        如果两个const的赋值语句的表达式是一样的，那么可以省略后一个赋值表达式。
+        const (                 // iota被重设为0
+            c0 = iota           // c0 = 0
+            c1                  // c1 = 1
+            c2                  // c2 = 2
+        )
+        const (                      // iota被重设为0
+            c0 = 1 << iota           // c0 = 1
+            c1                       // c1 = 2
+            c2                       // c2 = 4
+        )
+ */
+
+
+/*
+    枚举：
+        const (
+            Sunday = iota
+            Monday
+            Tuesday
+            Wednesday
+            Thursday
+            Friday
+            Saturday
+            numberOfDays
+        )
+ */
+
+
+/*
+    类型：
+    Go语言内置一下这些基础类型：
+    1， 布尔类型：bool
+    2， 整型：int8、byte、int16、int、uint、uintptr等
+    3， 浮点类型：float32、float64
+    4，
  */
