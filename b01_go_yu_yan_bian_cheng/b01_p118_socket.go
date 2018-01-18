2dece506bec153a428c22069ae7a7926758d6c25
zxwtry <zxwtry@qq.com>
2018-01-18 18:29:23 +0800

    add b01 p118 socket

---
+++ b01_go_yu_yan_bian_cheng/b01_p118_socket.go
@@ -0,0 +1,25 @@
+package b01_go_yu_yan_bian_cheng
+
+/**
+ * User:  zxwtry
+ * Date:  2018/1/18
+ * Time:  18:22
+ */
+
+
+
+/*
+    网络编程：
+        1，建立Socket
+        2，绑定Socket
+        3，监听
+        4，接受连接
+        5，接收
+
+        Go语言：net.Dial()
+
+
+    Dial()函数
+ */
+
+
